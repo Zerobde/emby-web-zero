0ab1123c486addcd00fe59e22f28224b31d54482
Zerobde <zero.gen.x@gmail.com>
2017-01-29 16:34:43 -0500

    Update movies.js

--- movies/movies.js
+++ movies/movies.js
@@ -236,8 +236,8 @@ define(['loading', 'alphaPicker', './../components/horizontallist', './../compon
                         ParentId: pageParams.parentid,
                         IncludeItemTypes: "Movie",
                         Recursive: true,
-                        SortBy: "SortName",
-                        Fields: "SortName"
+                        SortBy: "PremiereDate",
+                        Fields: "SortOrder"
                     });
                 },
                 listCountElement: page.querySelector('.listCount'),
