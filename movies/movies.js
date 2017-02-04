731c6ef4801bda781c89ee67864b1db22aa4ffc1
Zerobde <zero.gen.x@gmail.com>
2017-02-04 07:12:52 -0500

    Update movies.js

--- movies/movies.js
+++ movies/movies.js
@@ -170,7 +170,7 @@ define(['loading', 'alphaPicker', './../components/horizontallist', './../compon
                     cardOptions: {
                         indexBy: 'Genres',
                         genres: genresResult.Items,
-                        /*indexLimit: 3,*/
+                        indexLimit: 6,
                         parentId: pageParams.parentid,
                         rows: {
                             portrait: 2,
